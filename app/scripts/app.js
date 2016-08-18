a9ccf2aa2bc8229590eb4414493ae66533259883
Nick <nwarnke@unomaha.edu>
2016-08-18 17:36:12 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularExampleApp
+ * @description
+ * # angularExampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularExampleApp', []);
